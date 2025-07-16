01168a3d66dda11825a1c2a69664bcd8669d0e6d
ysang989 <67964576+ysang989@users.noreply.github.com>
2025-07-17 00:26:34 +0900

    [Bronze V] Title: 문자열, Time: 172 ms, Memory: 17332 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i = 0; i < n; i++) {
+            String s = sc.next();
+            System.out.print(s.charAt(0));
+            System.out.println(s.charAt(s.length() - 1));
+        }
+        sc.close();
+    }
+}
