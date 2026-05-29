3fa553774257c4e20b37fb03c3cbd42fdcfac37a
ysang989 <67964576+ysang989@users.noreply.github.com>
2026-05-29 23:20:29 +0900

    [Passed] 숫자의 증감 (127ms, 10MB)

---
+++ codetree/trail1/숫자의 증감/increasing-and-decreasing-numbers.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        char c = sc.next().charAt(0);
+        int n = sc.nextInt();
+
+        if (c == 'A') {
+            for (int i = 1; i <= n; i++){
+                System.out.print(i + " ");
+            }
+        } else if ( c == 'D') {
+            for (int i = n; i > 0; i--){
+                System.out.print(i + " ");
+            }
+        
+        }
+    }
+}
